8c43a83d380a5bfd202d59ce344c59ccbb89650a
VIton4ik <viton4ik@gmail.com>
2023-08-16 00:29:52 +0200

    filters

--- Project/App/views.py
+++ Project/App/views.py
@@ -214,17 +214,97 @@ def html_404(request):
 def indexPage(request):
     return render(request, 'App/index.html', {})
 
-
+from django.db.models import Q
 def mainPage(request):
 
     # vehicles=Vehicle.objects.filter().values_list("deliveryDate", flat=True)
     # vehicles=Vehicle.objects.filter().values("deliveryDate")
-    vehicles = Vehicle.objects.all().order_by('-deliveryDate')
-    user = request.user
-    print('user:', user)
-    print('user.id:', user.id)
-    print('vehicle:', vehicles)
 
+    # vehicles = Vehicle.objects.all().order_by('-deliveryDate')
+
+    # user = request.user
+    # print('user:', user)
+    # print('user.id:', user.id)
+    # print('vehicle:', vehicles)
+
+    #filters block
+
+    # Получение всех доступных моделей автомобилей. distinct() - убирает дубликаты из списка значений
+    # vehicle_models_ids = Vehicle.objects.values_list('vehicleModel', flat=True).distinct()
+    vehicle_models_names = VehicleModel.objects.values_list('name', flat=True).distinct()
+
+    # Получение значения фильтра из параметров запроса
+    filter_vehicle_model = request.GET.get('vehicle_model')
+    try:
+        vehicle_model_id = VehicleModel.objects.get(name=filter_vehicle_model)
+    except:
+        # vehicles = Vehicle.objects.all().order_by('-deliveryDate')
+        vehicle_model_id = ''
+
+
+    # Получение всех доступных моделей двигателей. 
+    engine_models_names = EngineModel.objects.values_list('name', flat=True).distinct()
+
+    filter_engine_model = request.GET.get('engine_model')
+    try:
+        engine_model_id = EngineModel.objects.get(name=filter_engine_model)
+    except:
+        engine_model_id = ''
+        # vehicles = Vehicle.objects.all().order_by('-deliveryDate')
+
+    
+    # Получение всех доступных Модель трансмиссии. 
+    transmission_models_names = TransmissionModel.objects.values_list('name', flat=True).distinct()
+
+    filter_transmission_model = request.GET.get('transmission_model')
+    try:
+        transmission_model_id = TransmissionModel.objects.get(name=filter_transmission_model)
+    except:
+        transmission_model_id = ''
+        # vehicles = Vehicle.objects.all().order_by('-deliveryDate')
+
+    # Создаем пустой Q-объект
+    q_filter = Q()
+
+    if filter_vehicle_model:
+        q_filter &= Q(vehicleModel=vehicle_model_id)
+    
+    if filter_engine_model:
+        q_filter &= Q(engineModel=engine_model_id)
+    
+    if filter_transmission_model:
+        q_filter &= Q(transmissionModel=transmission_model_id)
+
+    # Если есть фильтр, применяем его
+    # if filter_vehicle_model:
+    #     # vehicles = Vehicle.objects.filter(vehicleModel=filter_vehicle_model)
+    #     vehicles = Vehicle.objects.filter(vehicleModel=vehicle_model_id)
+
+
+    # resetFilterButton = request.GET.get('resetFilterButton')
+
+    # if resetFilterButton == "True":
+    #         vehicles = Vehicle.objects.all().order_by('-deliveryDate')
+            # print('resetFilterButton:', resetFilterButton)
+
+
+
+    if filter_vehicle_model or filter_engine_model or filter_transmission_model:
+    #     vehicles = Vehicle.objects.filter(vehicleModel=vehicle_model_id, engineModel=engine_model_id, transmissionModel=transmission_model_id)
+
+    # Применяем фильтры
+        vehicles = Vehicle.objects.filter(q_filter)
+        
+    # elif filter_engine_model:
+    #     vehicles = Vehicle.objects.filter(engineModel=engine_model_id)
+
+    else:
+        # vehicles = Vehicle.objects.all()
+        vehicles = Vehicle.objects.all().order_by('-deliveryDate')
+    
+
+
+    # pagination block
     # Получение страницы из параметров запроса
     page_number = request.GET.get('page')
 
@@ -234,6 +314,7 @@ def mainPage(request):
     # Получение объекта Page для текущей страницы
     page = paginator.get_page(page_number)
 
-    return render(request, 'App/main.html', {'vehicles': vehicles, 'page': page})
+
+    return render(request, 'App/main.html', {'vehicles': vehicles, 'page': page, 'vehicle_models_names': vehicle_models_names, 'selected_filter': filter_vehicle_model, 'engine_models_names': engine_models_names, 'filter_engine_model': filter_engine_model, 'transmission_models_names': transmission_models_names, 'filter_transmission_model': filter_transmission_model})
 
 
